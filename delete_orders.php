d87c0fba75bd6761f4a9c327394bc994357d5ff1
Manvi Godbole <118299024+Godbolemanvi@users.noreply.github.com>
2023-09-05 19:13:07 +0530

    Add files via upload

---
+++ delete_orders.php
@@ -0,0 +1,12 @@
+    <!--  * Author Name: Manvi Godbole. --><?php
+include("connection/connect.php"); //connection to db
+error_reporting(0);
+session_start();
+
+
+// sending query
+mysqli_query($db,"DELETE FROM users_orders WHERE o_id = '".$_GET['order_del']."'"); 
+header("location:your_orders.php"); 
+
+?>
+    <!--  * GigHub Link: https://github.com/Godbolemanvi -->
---
+++ logout.php
@@ -0,0 +1,11 @@
+<!--  * Author Name: Manvi Godbole.
+* GigHub Link: https://github.com/Godbolemanvi -->
+<?php
+session_start(); 
+session_destroy(); 
+$url = 'login.php';
+header('Location: ' . $url); 
+
+?>
+<!--  * Author Name: Manvi Godbole.
+* GigHub Link: https://github.com/Godbolemanvi -->
